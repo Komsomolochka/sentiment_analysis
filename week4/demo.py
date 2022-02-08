6fccbd5e185185640280e59f604e9387f77e2c24
Darya Strekalovskaya <51093169+Komsomolochka@users.noreply.github.com>
2022-02-08 18:28:05 +0300

    Add files via upload

---
+++ week4/demo.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from sentiment_classifier import SentimentClassifier
+import time
+
+app = Flask(__name__)
+
+print("Preparing classifier")
+start_time = time.time()
+classifier = SentimentClassifier()
+print("Classifier is ready")
+print(time.time() - start_time, "seconds")
+
+@app.route('/', methods = ['GET', 'POST'])
+def demo(text="", prediction_message=""):
+    if request.method == "POST":
+        text = request.form["text"]
+        prediction_message = classifier.get_prediction_message(text)
+    return render_template('demo.html', text=text, prediction_message=prediction_message)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug = True)
